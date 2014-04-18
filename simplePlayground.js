8e9814c0a3d3bc1c89e81306588f7a47ea8fe291
ByzantineFailure <byzanitnefailure@gmail.com>
2014-04-18 17:21:20 -0400

    Made a kind of functioning thing and site

--- simplePlayground.js
+++ simplePlayground.js
@@ -1,9 +1,6 @@
 function AppViewModel() {
 	var self = this;
 
-	self.startMessage = ko.observable("This is the header!");
-	self.endMessage = ko.observable("This is the footer!");
-	
 	self.iterateSource = ko.observable("1, 2, 3, 4, 5");
 	self.iterateList = ko.observableArray();
 
@@ -19,3 +16,4 @@ function AppViewModel() {
 };
 
 ko.applyBindings(new AppViewModel());
+
